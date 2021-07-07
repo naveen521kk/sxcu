9ab308aec6e394296cbd086b3c5305b401134faa
Naveen M K <naveen521kk@gmail.com>
2021-07-07 18:12:49 +0530

    test_api: use Pathlib

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,42 +1,42 @@
-
-import os
 import time
+from pathlib import Path
 
 import pytest
 import requests
 
 from sxcu import SXCU
 
-pathFile = os.path.dirname(os.path.abspath(__file__))
-img_loc = os.path.join(pathFile, "assets", "sharex.png")
-
+FILE_PATH = Path(__file__).parent
+IMG_LOC = Path(FILE_PATH, "assets", "sharex.png")
 
 
 @pytest.mark.slow
 def test_upload_keys_default_domain_and_delete_image() -> None:
     time.sleep(60)
 
-    t = SXCU()
-    con = t.upload_image(file=img_loc)
+    _t = SXCU()
+    con = _t.upload_image(file=IMG_LOC)
     expected_keys = ["url", "del_url", "thumb"]
     assert (
         list(con.keys()).sort() == expected_keys.sort()
     )  # sorting because keys are arraged different
 
-    a = SXCU.delete_image(con["del_url"])
-    assert a is True
+    _a = SXCU.delete_image(con["del_url"])
+    assert _a is True
+
 
 @pytest.mark.slow
 @pytest.mark.xfail(run=False, reason="sxcu optimises images")
 def test_upload_image_default_domain() -> None:
     t = SXCU()
-    con = t.upload_image(file=img_loc, noembed=True)
+    con = t.upload_image(file=IMG_LOC, noembed=True)
 
     con = requests.get(con["url"])
-    assert open(img_loc, "rb") == con.content
+    assert open(IMG_LOC, "rb") == con.content
 
 
 # TODO: Test subdomains
+
 
 @pytest.mark.slow
 def test_image_info() -> None:
@@ -44,7 +44,7 @@ def test_image_info() -> None:
     # upload image first
     t = SXCU()
     time.sleep(60)
-    con = t.upload_image(file=img_loc, noembed=True)
+    con = t.upload_image(file=IMG_LOC, noembed=True)
     details = SXCU.image_details(image_url=con["url"])
 
     assert con["url"] == details["url"]
@@ -61,13 +61,14 @@ def test_image_info() -> None:
     except AttributeError:
         assert True
 
+
 @pytest.mark.slow
 def test_sxcu_file_parser() -> None:
 
-    sxcu_file = os.path.join(pathFile, "assets", "sxcu.net - python.is-ne.at.sxcu")
+    sxcu_file = Path(FILE_PATH, "assets", "sxcu.net - python.is-ne.at.sxcu")
     time.sleep(60)
-    t = SXCU(file_sxcu=sxcu_file)
-    con = t.upload_image(file=img_loc, noembed=True)
+    _t = SXCU(file_sxcu=sxcu_file)
+    con = _t.upload_image(file=IMG_LOC, noembed=True)
 
     # test domain
     assert con["url"].startswith("https://python.is-ne.at")
@@ -76,13 +77,14 @@ def test_sxcu_file_parser() -> None:
     expected_keys = ["url", "del_url", "thumb"]
     assert list(con.keys()).sort() == expected_keys.sort()
 
+
 @pytest.mark.slow
 def test_sxcu_file_parser_no_argument() -> None:
 
-    sxcu_file = os.path.join(pathFile, "assets", "sxcu.net - why-am-i-he.re.sxcu")
+    sxcu_file = Path(FILE_PATH, "assets", "sxcu.net - why-am-i-he.re.sxcu")
     time.sleep(120)
     t = SXCU(file_sxcu=sxcu_file)
-    con = t.upload_image(file=img_loc, noembed=True)
+    con = t.upload_image(file=IMG_LOC, noembed=True)
 
     # test domain
     assert con["url"].startswith("https://why-am-i-he.re")
@@ -91,12 +93,14 @@ def test_sxcu_file_parser_no_argument() -> None:
     expected_keys = ["url", "del_url", "thumb"]
     assert list(con.keys()).sort() == expected_keys.sort()
 
+
 @pytest.mark.slow
 def test_upload_text() -> None:
     b = SXCU.upload_text("Hello, from sxcu Python Library. Test.")
     con = requests.get(b["url"])
 
     assert con.status_code == 200
+
 
 @pytest.mark.slow
 def test_list_subdomains() -> None:
@@ -107,6 +111,7 @@ def test_list_subdomains() -> None:
 
     assert list(b[0].keys()).sort() == req_keys.sort()
 
+
 @pytest.mark.slow
 def test_list_subdomains_all() -> None:
     b = SXCU.domain_list(-1)
@@ -114,9 +119,10 @@ def test_list_subdomains_all() -> None:
 
     assert list(b[0].keys()).sort() == req_keys.sort()
 
+
 @pytest.mark.slow
 def test_create_link() -> None:
-    t = SXCU()
-    con = t.create_link("https://github.com/naveen521kk/sxcu")
-    c = requests.get(con["url"])
-    assert c.status_code == 200
+    _t = SXCU()
+    con = _t.create_link("https://github.com/naveen521kk/sxcu")
+    _c = requests.get(con["url"])
+    assert _c.status_code == 200
