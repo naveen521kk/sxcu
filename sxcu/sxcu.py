6edb9ed6f0071012b89e1c30cdab96069112f6fc
Naveen M K <naveen521kk@gmail.com>
2021-07-07 17:39:35 +0530

    Move og_properties to a new file

--- sxcu/sxcu.py
+++ sxcu/sxcu.py
@@ -12,68 +12,11 @@ from .constants import (
     status_code_upload_text,
 )
 from .exceptions import SXCUError
+from .og_properties import OGProperties
 
-__all__ = ["OGProperties", "SXCU"]
+__all__ = ["SXCU"]
 
 request_handler = RequestClient()
-
-
-class OGProperties:
-    """
-    This is a helper class for main SXCU function. This helps you to reuse
-    the :class:`OGProperties`.
-    """
-
-    def __init__(
-        self,
-        color: str = None,
-        description: str = None,
-        title: str = None,
-        discord_hide_url: bool = False,
-    ) -> None:
-        self.color = color
-        self.description = description
-        self.title = title
-        self.discord_hide_url = discord_hide_url
-
-    def export(self) -> str:
-        """Exports the Property set to a JSON file.
-
-        Returns
-        =======
-        :class:`str`
-            Using ``json.dumps`` the content of JSON file is dumped.
-        """
-        return json.dumps(
-            {
-                "color": self.color,
-                "title": self.title,
-                "description": self.description,
-                "discord_hide_url": self.discord_hide_url,
-            }
-        )
-
-    @classmethod
-    def from_json(cls, contents: str) -> "OGProperties":
-        """Import the Property set from parsing JSON.
-
-        Parameters
-        ==========
-        contents: :class:`str`
-            The contents in JSON which needs to be parsed.
-
-        Returns
-        =======
-        :class:`str`
-            Using ``json.dumps`` the content of JSON file is dumped.
-        """
-        _dict = json.loads(contents)
-
-        color = _dict["color"]
-        description = _dict["description"]
-        title = _dict["title"]
-        discord_hide_url = _dict["discord_hide_url"]
-        return cls(color, description, title, discord_hide_url)
 
 
 class SXCU:
