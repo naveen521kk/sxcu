a3e43a2269b57aee3cbe2ec92d2d0d2fdb85a37a
Naveen M K <naveen521kk@gmail.com>
2021-12-19 14:27:16 +0530

    Don't add a logging handler
    That should be done by the user and not the library.

--- sxcu/__logger__.py
+++ sxcu/__logger__.py
@@ -10,10 +10,3 @@ __all__ = ["logger"]
 import logging
 
 logger = logging.getLogger("sxcu")  # pylint: disable=invalid-name
-S_HANDLER = logging.StreamHandler()
-S_HANDLER.setLevel(logging.DEBUG)
-S_FORMAT = logging.Formatter(
-    "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-)
-S_HANDLER.setFormatter(S_FORMAT)
-logger.addHandler(S_HANDLER)
