b09192e442502b41dc803bea1ee7790ac835af64
Naveen M K <naveen521kk@gmail.com>
2021-12-19 14:22:03 +0530

    Fix typos in `__client__.py`

--- sxcu/__client__.py
+++ sxcu/__client__.py
@@ -20,7 +20,7 @@ class RequestClient:
     """
 
     def __init__(self, headers: dict = None) -> None:
-        """This initaite the handlers.
+        """This initiate the handlers.
         Parameters
         ==========
         headers : :class:`dict`, optional
@@ -31,13 +31,13 @@ class RequestClient:
             self.headers = headers
         else:
             self.headers = HEADERS
-        logger.debug("Headers recieved are: %s", self.headers)
+        logger.debug("Request Headers: %s", self.headers)
 
     def post(
         self, url: str, headers: dict = None, **kwargs  # noqa ANN003
     ) -> requests.models.Response:
         """Pass all the parameter to :func:`requests.post`.
-        Also, adding the neccessary headers. Also, the newly passed header
+        Also, adding the necessary headers. Also, the newly passed header
         would overide the default.
         Parameters
         ==========
@@ -49,20 +49,20 @@ class RequestClient:
                     The header would overide the default header.
 
         """
-        logger.debug("Trying to do a Post Requests to %s", url)
+        logger.debug("Post Requests to: %s", url)
         headers = self.headers if headers is None else headers
         con = requests.post(url, headers=headers, **kwargs)
-        logger.debug("Recieved Headers from %s: %s", url, con.headers)
+        logger.debug("Received Headers from %s: %s", url, con.headers)
         logger.debug("status_code returned was:%s", con.status_code)
         response = con.text
-        logger.info("Recieved Response: %s", response)
+        logger.info("Received Response: %s", response)
         return con
 
     def get(
         self, url: str, headers: dict = None, **kwargs  # noqa ANN003
     ) -> requests.models.Response:
         """Pass all the parameter to :func:`requests.get`.
-        Also, adding the neccessary headers. Also, the newly passed header
+        Also, adding the necessary headers. Also, the newly passed header
         would overide the default.
         Parameters
         ==========
@@ -74,11 +74,11 @@ class RequestClient:
                     The header would overide the default header.
 
         """
-        logger.debug("Trying to do Get Requests to %s", url)
+        logger.debug("Get Requests to %s", url)
         headers = self.headers if headers is None else headers
         con = requests.get(url=url, headers=headers, **kwargs)
-        logger.debug("Recieved Headers from %s: %s", url, con.headers)
+        logger.debug("Received Headers from %s: %s", url, con.headers)
         # Don't use json instead implement a custom class here?
         response = con.text
-        logger.info("Recieved Response: %s", response)
+        logger.info("Received Response: %s", response)
         return con
