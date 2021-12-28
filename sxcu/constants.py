d5cc39b9ac951f2897edc0ec64d2f488f118bcef
Naveen M K <naveen521kk@gmail.com>
2021-12-28 19:20:33 +0530

    Remove useless stuff in `DefaultDomains`

--- sxcu/constants.py
+++ sxcu/constants.py
@@ -19,12 +19,7 @@ class DefaultDomains(Enum):
     """DefaultDomains A Emum representing all the default API
     URL's to use.
     """
-
-    DOMAINS_LIST = "https://sxcu.net/api/subdomain"
-    IMAGE_DETAILS = "https://sxcu.net/api/files/{image_id}"
     UPLOAD_TEXT = "https://cancer-co.de/upload"
-    COLLECTION_DETAILS = "https://sxcu.net/api/collections/{collection_id}"
-    EDIT_COLLECTIONS = "https://sxcu.net/api/collections/edit/{collection_id}"
     API_ENDPOINT = "https://sxcu.net/api/"
 
 HEADERS = {"User-Agent": f"PySXCU/{__version__} (https://pypi.org/project/sxcu/)"}
