ba79b694ab1b4721890077a8c1ff19af7ecc3c18
Naveen M K <naveen521kk@gmail.com>
2022-02-17 17:38:26 +0530

    Format files

--- sxcu/sxcu.py
+++ sxcu/sxcu.py
@@ -147,7 +147,7 @@ class SXCU:
             files = {"file": open(file, "rb")}
         res = request_handler.post(url, files=files, data=data)
         if not isinstance(file, io.BytesIO):
-            files['file'].close()
+            files["file"].close()
         if res.status_code != SXCU_SUCCESS_CODE:
             error_response = res.json()
             raise_error(
